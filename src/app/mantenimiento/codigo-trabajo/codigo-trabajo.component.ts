bc35f4b7679c62eaebbaa198c9b3f84a07e5de2c
Ivan Cruz <ivancruzhn@gmail.com>
2022-03-15 06:04:16 -0600

    Mantenimiento Empleado completado

---
+++ src/app/mantenimiento/codigo-trabajo/codigo-trabajo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-codigo-trabajo',
+  templateUrl: './codigo-trabajo.component.html',
+  styleUrls: ['./codigo-trabajo.component.css']
+})
+export class CodigoTrabajoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
